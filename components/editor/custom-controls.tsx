111fc8673d32a69854d299fb2c9dd449a0d936ad
Aymeric Rabot <aymeric.rabot@gmail.com>
2025-10-22 22:11:36 -0400

    Restricting left click panning in other control modes

--- components/editor/custom-controls.tsx
+++ components/editor/custom-controls.tsx
@@ -3,14 +3,11 @@
 import { useEditor } from '@/hooks/use-editor'
 import { CameraControls, CameraControlsImpl } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
-import { useEffect } from 'react'
-
-type CustomControlsProps = {
-  tileSize: number
-}
+import { useEffect, useMemo } from 'react'
 
 
-export function CustomControls({ tileSize }: CustomControlsProps) {
+
+export function CustomControls() {
   const controlMode = useEditor((state) => state.controlMode);
   const controls = useThree((state) => state.controls);
 
@@ -21,17 +18,36 @@ export function CustomControls({ tileSize }: CustomControlsProps) {
     (controls as CameraControlsImpl).setLookAt(10, 10, 10, 0, 0, 0, true);
   }, [controls]);
 
+  // Configure mouse buttons based on control mode
+  const mouseButtons = useMemo(() => {
+    // In select mode, left-click can pan the camera
+    if (controlMode === 'select') {
+      return {
+        left: CameraControlsImpl.ACTION.TRUCK,
+        middle: CameraControlsImpl.ACTION.SCREEN_PAN,
+        right: CameraControlsImpl.ACTION.ROTATE,
+        wheel: CameraControlsImpl.ACTION.DOLLY,
+      };
+    }
+
+    // In delete, build, and guide modes, disable left-click for camera
+    // (reserved for mode-specific actions)
+    return {
+      left: CameraControlsImpl.ACTION.NONE,
+      middle: CameraControlsImpl.ACTION.SCREEN_PAN,
+      right: CameraControlsImpl.ACTION.ROTATE,
+      wheel: CameraControlsImpl.ACTION.DOLLY,
+    };
+  }, [controlMode]);
+
   return (
     <CameraControls
       minPolarAngle={0}
       maxPolarAngle={Math.PI / 2 - 0.1}
       minDistance={10}
       maxDistance={50}
-    mouseButtons={{
-      left: CameraControlsImpl.ACTION.TRUCK,
-      middle: CameraControlsImpl.ACTION.SCREEN_PAN,
-      right: CameraControlsImpl.ACTION.ROTATE,
-      wheel: CameraControlsImpl.ACTION.DOLLY,
-    }} makeDefault/>
+      mouseButtons={mouseButtons}
+      makeDefault
+    />
   )
 }
