b4d799da9798237fe9b3d5131b790b8d0b59e25f
Aymeric Rabot <aymeric.rabot@gmail.com>
2025-10-29 11:12:44 -0400

    Add vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ReactScan } from '@/components/debug/react-scan'
 
 const geistSans = Geist({
@@ -27,6 +29,8 @@ export default function RootLayout({
     <html lang="en">
       <ReactScan />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
