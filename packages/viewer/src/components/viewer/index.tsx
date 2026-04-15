f1d60caf22cd14fa9275edf33a810f458db12055
sudhir <sudhir9297@gmail.com>
2026-04-15 10:48:16 +0530

    Fix WebGPU renderer initialization order

--- packages/viewer/src/components/viewer/index.tsx
+++ packages/viewer/src/components/viewer/index.tsx
@@ -106,11 +106,11 @@ const Viewer: React.FC<ViewerProps> = ({
       camera={{ position: [50, 50, 50], fov: 50 }}
       className={`transition-colors duration-700 ${theme === 'dark' ? 'bg-[#1f2433]' : 'bg-[#fafafa]'}`}
       dpr={[1, 1.5]}
-      gl={(props) => {
+      gl={async (props) => {
         const renderer = new THREE.WebGPURenderer(props as any)
         renderer.toneMapping = THREE.ACESFilmicToneMapping
         renderer.toneMappingExposure = 0.9
-        // renderer.init() // Only use when using <DebugRenderer />
+        await renderer.init()
         return renderer
       }}
       resize={{
