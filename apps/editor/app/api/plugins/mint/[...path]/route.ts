c04b2c220f0bc3f50ca4617a9eb6627ab8bf042f
Tamrat <tamg@users.noreply.github.com>
2026-08-04 19:09:05 -0400

    Harden Mint plugin host integration

--- apps/editor/app/api/plugins/mint/[...path]/route.ts
+++ apps/editor/app/api/plugins/mint/[...path]/route.ts
@@ -1,8 +1,9 @@
 import { handleMintPascalRequest } from '@mint/pascal-plugin/server'
+import { BASE_URL } from '@/lib/utils'
 
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 
-const route = (request: Request) => handleMintPascalRequest(request)
+const route = (request: Request) => handleMintPascalRequest(request, { origin: BASE_URL })
 
 export { route as GET, route as POST }
